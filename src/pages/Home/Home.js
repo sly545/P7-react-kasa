1a8b1ae8ef5c15102dc9755ed0afc00e360e745b
sly545 <sylvainferrandes@gmail.com>
2023-03-24 16:46:45 +0100

    ajout de la galery

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Banner from "../../components/Banner/Banner"
-
+import Card from "../../components/card/card"
 
  
 export default function Home() {
     return (
         <div> 
         <Banner/>
+        <Card/>
          </div>
     )
 }
