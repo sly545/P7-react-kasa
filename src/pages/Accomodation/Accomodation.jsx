b9b35795b5a91dec11c31809264ba274e1491e1e
sly545 <sylvainferrandes@gmail.com>
2023-03-28 01:45:24 +0200

    ils manquait des truc de l'api pour la page logement

--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -1,23 +1,30 @@
+// Import de useParams depuis React Router et useState et useEffect depuis React
 import { useParams } from 'react-router-dom';
-
 import { useState, useEffect } from 'react';
 
-
+// Définition du composant Accomodation
 export default function Accomodation() {
+  // Récupération de l'ID de l'hébergement depuis l'URL avec useParams
   const { id } = useParams();
+
+  // Définition de l'état local accomodations avec useState, initialisé à une liste vide
   const [accomodations, setAccomodations] = useState([]);
 
+  // Utilisation de useEffect pour charger les données depuis le fichier JSON
   useEffect(() => {
-    fetch('http://localhost:3000/data.json')
-      .then(response => response.json())
-      .then(data => setAccomodations(data));
-  }, []);
+    fetch('http://localhost:3000/data.json') // Appel à l'API fetch pour récupérer les données
+      .then(response => response.json()) // Conversion de la réponse en JSON
+      .then(data => setAccomodations(data)); // Stockage des données dans l'état local avec setAccomodations
+  }, []); // Dépendance vide pour exécuter cet effet uniquement au montage du composant
 
+  // Récupération de l'hébergement correspondant à l'ID spécifié avec la méthode find
   const accomodation = accomodations.find(a => a.id === id);
 
+  // Rendu du composant
   return (
     <div>
       <h2>Appartement sélectionné : {id}</h2>
+      {/* Affichage des détails de l'hébergement s'il est trouvé */}
       {accomodation && (
         <div>
           <h3>{accomodation.title}</h3>
@@ -28,6 +35,10 @@ export default function Accomodation() {
           <p>Note : {accomodation.rating}</p>
           <p>Hôte : {accomodation.host.name}</p>
           <img src={accomodation.cover} alt={accomodation.title} />
+          {accomodation.pictures.map(picture => (
+            <img key={picture} src={picture} alt={accomodation.title} />
+          ))}
+          <img src={accomodation.host.picture} alt={accomodation.host.name} />
         </div>
       )}
     </div>
