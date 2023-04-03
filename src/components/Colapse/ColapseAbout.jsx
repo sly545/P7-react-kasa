1053ee964d203e507be46d2353e6239decd02035
sly545 <sylvainferrandes@gmail.com>
2023-04-04 00:36:16 +0200

    avencement de la partie apropos et avencement avec ajout de props

--- src/components/Colapse/ColapseAbout.jsx
+++ src/components/Colapse/ColapseAbout.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import styles from '../Colapse/Colapse.module.css';
 import PropTypes from 'prop-types';
 
-
-
 function CollapseSection({ title, description, fleshTop, fleshBottom }) {
   const [showDescription, setShowDescription] = useState(false);
 
@@ -12,16 +10,19 @@ function CollapseSection({ title, description, fleshTop, fleshBottom }) {
   };
 
   return (
-    <div>
-      <div className={styles.TitleContener}>
-        <h2 className={styles.descriptTitle}>
-          {title}
-          <img
-            src={showDescription ? fleshTop : fleshBottom}
-            onClick={handleFleshClick}
-          />
-        </h2>
+    <div className={styles.WrapAbout}>
+      <div className={styles.ContenerAbout} style={{backgroundColor: showDescription ? '#f1f1f1' : 'initial'}}> 
+        <div className={styles.TitleContener}>
+          <h2 className={styles.descriptTitle}>
+            {title}
+            <img
+              src={showDescription ? fleshTop : fleshBottom}
+              onClick={handleFleshClick}
+            />
+          </h2>
+        </div>
       </div>
+
       {showDescription && <h3 className={styles.descript}>{description}</h3>}
     </div>
   );
@@ -32,6 +33,6 @@ CollapseSection.propTypes = {
     description: PropTypes.string.isRequired,
     fleshTop: PropTypes.string.isRequired,
     fleshBottom: PropTypes.string.isRequired,
-  };
+};
 
 export default CollapseSection;
--- src/pages/APropos/APropos.jsx
+++ src/pages/APropos/APropos.jsx
@@ -13,6 +13,26 @@ export default function Apropos() {
   fleshTop={FleshTop}
   fleshBottom={FleshBottom}
 />
-    </div>
+<CollapseSection
+  title="Respect"
+  description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  fleshTop={FleshTop}
+  fleshBottom={FleshBottom}
+/>
+<CollapseSection
+  title="Service"
+  description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+  fleshTop={FleshTop}
+  fleshBottom={FleshBottom}
+/>
+<CollapseSection
+  title="Sécurité"
+  description="Sécurité
+  La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.."
+  fleshTop={FleshTop}
+  fleshBottom={FleshBottom}
+/>
+</div>
+    
   );
 }
