85475bef0dacaf9d4fb510bd46c14ee183d18eb3
sly545 <sylvainferrandes@gmail.com>
2023-04-03 10:06:23 +0200

    ajout du composent dans les props de colapse pour les equipement

--- src/components/Colapse/Colapse.jsx
+++ src/components/Colapse/Colapse.jsx
@@ -6,29 +6,57 @@ import styles from '../Colapse/Colapse.module.css'
 
 export function Colapse(props) {
   const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  const [isEquipmentsVisible, setIsEquipmentsVisible] = useState(false);
   const description = props.accomodation.description;
+  const equipments = props.accomodation.equipments;
 
   const handleToggleDescription = () => {
     setIsDescriptionVisible(!isDescriptionVisible);
   };
 
+  const handleToggleEquipments = () => {
+    setIsEquipmentsVisible(!isEquipmentsVisible);
+  };
+
   return (
-  <div className={styles.wrap} > 
     <div className={styles.contener}>
-      <div className={styles.TitleContener}>
-      <h2 className={styles.descriptTitle} onClick={handleToggleDescription}>
-        Description
-        <img className={styles.flesh} src={isDescriptionVisible ? FleshTop : FleshBottom} alt="toggle description" />
-      </h2>
-      </div>
-      {isDescriptionVisible && <h3 className={styles.descript} >{description}</h3>}
+      {props.showDescription && (
+        <div>
+          <div className={styles.TitleContener}>
+            <h2 className={styles.descriptTitle} onClick={handleToggleDescription}>
+              Description
+              <img className={styles.flesh} src={isDescriptionVisible ? FleshTop : FleshBottom} alt="toggle description" />
+            </h2>
+          </div>
+          {isDescriptionVisible && <h3 className={styles.descript} >{description}</h3>}
+        </div>
+      )}
+ 
+      {props.showEquipments && (
+        <div>
+          <div className={styles.TitleContener}>
+            <h2 className={styles.descriptTitle} onClick={handleToggleEquipments}>
+              Equipements
+              <img className={styles.flesh} src={isEquipmentsVisible ? FleshTop : FleshBottom} alt="toggle equipments" />
+            </h2>
+          </div>
+          {isEquipmentsVisible && (
+            <ul className={styles.descript} >
+              {equipments.map((equipment, index) => (
+                <li  key={index}>{equipment}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
     </div>
-  </div>
   );
 }
 
 Colapse.propTypes = {
   accomodation: PropTypes.object.isRequired,
+  showDescription: PropTypes.bool,
+  showEquipments: PropTypes.bool,
 };
 
 export default Colapse;
--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -18,38 +18,30 @@ export default function Accomodation() {
 
   return (
     <div>
-     
       {accomodation && (
-        <div>
+        <div className={styles.Wrap}>
           <Manege accomodation={accomodation}/>
-          
           <div className={styles.conteneurWrap} >
-              <div className={styles.wrapGauche}>
-                <div>
-                  <h3 className={styles.title} >{accomodation.title}</h3>
-                  <p className={styles.lieu} >{accomodation.location}</p>
-                </div>
-
-            <ul className={styles.cartier}>
-              {accomodation.tags.map((equipment, index) => (
-               <li className={styles.listCartier} key={index}>{equipment}</li>
-              ))}
-            </ul>
-                <Colapse accomodation={accomodation} />
+            <div className={styles.wrapGauche}>
+              <div>
+                <h3 className={styles.title} >{accomodation.title}</h3>
+                <p className={styles.lieu} >{accomodation.location}</p>
               </div>
-
-              <div className={styles.wrapDroite}>   
-                <p>{accomodation.host.name}</p>
-                <img src={accomodation.host.picture} alt={accomodation.host.name} />
-                <p>{accomodation.rating}</p>
-            <ul>
-              {accomodation.equipments.map((equipment, index) => (
-               <li className={styles.listeEquipe} key={index}>{equipment}</li>
-              ))}
-            </ul>
+              <ul className={styles.cartier}>
+                {accomodation.tags.map((equipment, index) => (
+                  <li className={styles.listCartier} key={index}>{equipment}</li>
+                ))}
+              </ul>
+              <Colapse accomodation={accomodation} showDescription />
+            </div>
+            <div className={styles.wrapDroite}>   
+              <p>{accomodation.host.name}</p>
+              <img src={accomodation.host.picture} alt={accomodation.host.name} />
+              <p>{accomodation.rating}</p>
+              <Colapse accomodation={accomodation} showEquipments />
             </div>
           </div>
-      </div>
+        </div>
       )}
     </div>
   );
