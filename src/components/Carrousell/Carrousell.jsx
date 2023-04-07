143a587fdec6824aa26b21a5cf14042b7c3fa33e
sly545 <sylvainferrandes@gmail.com>
2023-04-07 23:37:30 +0200

    ajout du compteur dans le carrousell et de la condition si le chemein n'est pas galerer pour ne pas avoir l'avertisement

--- src/components/Carrousell/Carrousell.jsx
+++ src/components/Carrousell/Carrousell.jsx
@@ -1,15 +1,13 @@
-import { useState,} from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import styles from '../Carrousell/Carrousell.module.css'
 import fleshLeft from '../../assets/felshLeft.png'
 import fleshRight from '../../assets/flesheRight.png' 
 
-
 function Carrousell(props) {
   const photos = props.accomodation.pictures;
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
 
- 
   const handleNextPhoto = () => {
     setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
   };
@@ -20,10 +18,13 @@ function Carrousell(props) {
 
   return (
     <div className={styles.WrapManege}>
-    <img className={styles.buttonLeft} src={fleshLeft} onClick={handlePrevPhoto} alt="Précédent" />
-    <img className={styles.photoCarousel} src={photos[currentPhotoIndex]} alt="" />
-    <img className={styles.buttonRight} src={fleshRight} onClick={handleNextPhoto} alt="Suivant" />
-  </div>
+      <img className={styles.buttonLeft} src={fleshLeft} onClick={handlePrevPhoto} alt="Précédent" />
+      <img className={styles.photoCarousel} src={photos[currentPhotoIndex]} alt="" />
+      <div className={styles.counter}>
+        {currentPhotoIndex + 1} / {photos.length}
+      </div>
+      <img className={styles.buttonRight} src={fleshRight} onClick={handleNextPhoto} alt="Suivant" />
+    </div>
   );
 }
 
--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Carousell from '../../components/Carrousell/Carrousell';
 import styles from '../Accomodation/Accomodation.module.css';
 import Proprio from '../../components/Proprio/Propio';
@@ -7,53 +7,67 @@ import Colapse from '../../components/Colapse/Colapse';
 
 export default function Accomodation() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [accomodations, setAccomodations] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:3000/data.json')
       .then(response => response.json())
-      .then(data => setAccomodations(data));
+      .then(data => {
+        setAccomodations(data);
+        setIsLoading(false);
+      });
   }, []);
 
   const accomodation = accomodations.find(a => a.id === id);
 
+  useEffect(() => {
+    if (!isLoading && !accomodation) {
+      navigate('/notfound');
+    }
+  }, [isLoading, accomodation, navigate]);
+
+  if (isLoading || !accomodation) {
+    return null;
+  }
+
   return (
     <div>
-      {accomodation && (
-        <div className={styles.Wrap}>
-          <Carousell accomodation={accomodation} />
-          
-          <div className={styles.conteneurWrap}>
+      <div className={styles.Wrap}>
+        <Carousell accomodation={accomodation} />
+        <div className={styles.conteneurWrap}>
+          <div className={styles.contenerProprio}>
+            <div className={styles.apart}>
+              <div className={styles.locali}>
+                <span>
+                  <h3 className={styles.title}>{accomodation.title}</h3>
+                  <p className={styles.lieu}>{accomodation.location}</p>
+                </span>
+              </div>
+              <div className={styles.uli}>
+                <ul className={styles.cartier}>
+                  {accomodation.tags.map((equipment, index) => (
+                    <li className={styles.listCartier} key={index}>
+                      {equipment}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
 
-            <div className={styles.contenerProprio}>
-             
-            <div className={styles.apart}> 
-            <div className={styles.locali}> 
-              <span><h3 className={styles.title}>{accomodation.title}</h3>
-              <p className={styles.lieu}>{accomodation.location}</p></span>
-            </div>
-            <div className={styles.uli}>
-              <ul className={styles.cartier}>
-                {accomodation.tags.map((equipment, index) => (
-                  <li className={styles.listCartier} key={index}>
-                    {equipment}
-                  </li>
-                  
-                ))}
-              </ul>
-            </div>
-             </div>
-            
             <div className={styles.propri}>
               <Proprio accomodation={accomodation} />
             </div>
-            </div>
-            <div className={styles.trop}>
+          </div>
+          <div className={styles.trop}>
             <div className={styles.contenerEquipeDescript}>
               <div className={styles.tailleDes}>
-              
-<Colapse title="description" content={accomodation.description} hasHeight={true} />
-              
+                <Colapse
+                  title="description"
+                  content={accomodation.description}
+                  hasHeight={true}
+                />
               </div>
               <div className={styles.tailleEqui}>
                 <Colapse
@@ -69,10 +83,9 @@ export default function Accomodation() {
                 />
               </div>
             </div>
-            </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
