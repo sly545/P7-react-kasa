d8dab7418e58521049aff06e12d5deb001ef98ea
sly545 <sylvainferrandes@gmail.com>
2023-04-15 11:36:18 +0200

    ajout de fontawsomme et reglage nom padding

--- src/components/Proprio/Propio.jsx
+++ src/components/Proprio/Propio.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-import EtoileFull from '../../assets/Etoileplein.svg'
-import EtoileEmpty from '../../assets/Etoilevide.svg'
-import styles from '../Proprio/Proprio.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons';
+import styles from '../Proprio/Proprio.module.css';
 
 function Proprio(props) {
   // Utilisation de la fonction useState pour définir les variables name, picture et rating à partir des propriétés de l'objet accomodation passé en tant que prop
-  const [name] = useState(props.accomodation.host.name);
-  const [picture] = useState(props.accomodation.host.picture);
-  const [rating] = useState(props.accomodation.rating);
+  const {rating} = props.accomodation;
+  const {name,picture } = props.accomodation.host;
 
   // Création d'un tableau vide pour stocker les étoiles
   const stars = [];
   
   // Boucle for pour générer les étoiles pleines en fonction de la valeur de rating
   for (let i = 0; i < rating; i++) {
-    // Ajout d'une étoile pleine au tableau stars  j'ai ajouter une classe pour une media query pour les plus petit smartphones. 
-    stars.push(<img className={styles.etoilePlein} src={EtoileFull} alt="étoile pleine" key={i} />);
+    // Ajout d'une étoile pleine au tableau stars j'ai ajouter une classe pour une media query pour les plus petit smartphones. 
+    stars.push(<FontAwesomeIcon icon={fullStar} className={styles.etoilePlein} key={i} />);
   }
   
   // Boucle for pour générer les étoiles vides
   for (let i = rating; i < 5; i++) {
     // Ajout d'une étoile vide au tableau stars j'ai ajouter une classe pour une media query pour les plus petit smartphones. 
-    stars.push(<img className={styles.etoileVide} src={EtoileEmpty} alt="étoile vide" key={i} />);
+    stars.push(<FontAwesomeIcon icon={fullStar} className={styles.etoileVide} key={i} />);
   }
 
   return (
